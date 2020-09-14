6f52781a1763a506ada164f6d202c56fcdfd1110
piyushsrii <piyushnamo888@gmail.com>
2020-09-14 15:39:17 +0530

    [Piyush]Adding : Creating Board

--- TiktakToeGame.java
+++ TiktakToeGame.java
@@ -1,1 +1,30 @@
 #Welcome in tiktak toe#
+package tiktaktoe;
+
+/**
+ *
+ * @author user
+ */
+public class UserCase1 {
+
+     public static void main(String[] args) {
+
+        char[][] gameBoard={{' ','|',' ','|',' '},
+             {'-','+','-','+','-'},
+             {' ','|',' ','|',' '},
+             {'-','+','-','+','-'},
+             {' ','|',' ','|',' '}};
+
+       printGameBoard(gameBoard);
+}
+      public static void printGameBoard(char[][] gameBoard){
+
+        for(char[] a: gameBoard){
+            for(char b: a){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+
+}
+}
