dc0fda97528dee9e12458b487f1b509ac9817369
piyushsrii <piyushnamo888@gmail.com>
2020-09-14 15:49:45 +0530

    [Piyush]Adding : Choose X or O by palyer

--- TiktakToeGame.java
+++ TiktakToeGame.java
@@ -16,7 +16,67 @@ public class UserCase1 {
              {' ','|',' ','|',' '}};
 
        printGameBoard(gameBoard);
+
+       checkToss();
+       while(true)
+       {
+       System.out.println("Enter the between 1-9");
+       int playerPos=scan.nextInt();
+       System.out.println(""+playerPos);
+
+       placePeace(gameBoard,playerPos,"player");
+
+       Random rand=new Random();
+       int cpuPos=rand.nextInt(9)+1;
+       placePeace(gameBoard,cpuPos,"cpu");
+
+       printGameBoard(gameBoard);
+        }
+
 }
+         public static void placePeace(char[][] gameBoard,int pos, String user)
+       {
+        char symbol=' ';
+        if(user.equals("player")){
+            symbol='X';
+        }
+        else if(user.equals("cpu")){
+            symbol='O';
+        }
+      switch(pos){
+        case 1:
+            gameBoard[0][0]=symbol;
+            break;
+            case 2:
+            gameBoard[0][2]=symbol;
+            break;
+            case 3:
+            gameBoard[0][4]=symbol;
+            break;
+            case 4:
+            gameBoard[2][0]=symbol;
+            break;
+            case 5:
+            gameBoard[2][2]=symbol;
+            break;
+            case 6:
+            gameBoard[2][4]=symbol;
+            break;
+            case 7:
+            gameBoard[4][0]=symbol;
+            break;
+            case 8:
+            gameBoard[4][2]=symbol;
+            break;
+            case 9:
+            gameBoard[4][4]=symbol;
+            break;
+            default:
+                    break;
+       }
+      printGameBoard(gameBoard);        
+       }
+
       public static void printGameBoard(char[][] gameBoard){
 
         for(char[] a: gameBoard){
