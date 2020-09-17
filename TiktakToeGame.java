ec2e42edad950d13d26f22d7557a7a70cdc45426
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-17 14:36:07 +0530

    [Piyush] Modified : Remove unwanted spaces

--- TiktakToeGame.java
+++ TiktakToeGame.java
@@ -7,35 +7,34 @@ package tiktaktoe;
  */
 public class UserCase1 {
 
-     public static void main(String[] args) {
-
-        char[][] gameBoard={{' ','|',' ','|',' '},
+     public static void main(String[] args) 
+     {
+             char[][] gameBoard={{' ','|',' ','|',' '},
              {'-','+','-','+','-'},
              {' ','|',' ','|',' '},
              {'-','+','-','+','-'},
              {' ','|',' ','|',' '}};
-
-       printGameBoard(gameBoard);
-}
+             printGameBoard(gameBoard);
+      }
       public static void printGameBoard(char[][] gameBoard){
 
-        for(char[] a: gameBoard){
-            for(char b: a){
-                System.out.print(b);
-            }
-            System.out.println();
-        }
-      }
-         public static void checkToss()
-    {    
-        p1=scan.nextInt(2);
-        if(p1==0)
-        {
+          for(char[] a: gameBoard){
+                        for(char b: a){
+                              System.out.print(b);
+                                      }
+                                     System.out.println();
+                                       }
+                                       }
+          public static void checkToss()
+          {    
+            p1=scan.nextInt(2);
+            if(p1==0)
+            {
             System.out.println("p1 play first");
             System.out.println("p2 play Second");
-        }    
-        else
-        {
+            }    
+            else
+            {
             System.out.println("p2 play first");
             System.out.println("p1 play second");
         }
