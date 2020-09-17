9aa637ed3cb1f577470951b5b5c9021162bbeb3c
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-17 14:20:52 +0530

    [Piyush]  Modified : Remove unwanted spaces

--- TiktakToeGame.java
+++ TiktakToeGame.java
@@ -5,26 +5,24 @@ package tiktaktoe;
  *
  * @author user
  */
-public class UserCase1 {
-
-     public static void main(String[] args) {
-
-        char[][] gameBoard={{' ','|',' ','|',' '},
+public class UserCase1 
+{
+     public static void main(String[] args)
+     {
+             char[][] gameBoard={{' ','|',' ','|',' '},
              {'-','+','-','+','-'},
              {' ','|',' ','|',' '},
              {'-','+','-','+','-'},
              {' ','|',' ','|',' '}};
-
-       printGameBoard(gameBoard);
-}
-      public static void printGameBoard(char[][] gameBoard){
-
-        for(char[] a: gameBoard){
-            for(char b: a){
-                System.out.print(b);
-            }
-            System.out.println();
-        }
-
-}
+             printGameBoard(gameBoard);
+     }
+     public static void printGameBoard(char[][] gameBoard)
+     {
+            for(char[] a: gameBoard){
+                       for(char b: a){
+                             System.out.print(b);
+                                     }
+                                System.out.println();
+                                     }
+      }
 }
